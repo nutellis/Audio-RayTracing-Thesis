9e5fe740175f4d8e237d280438293ff9121563ed
Martino Pagliarani <martipaglia02@gmail.com>
2026-05-07 16:32:34 +0200

    refactor: remove ButterBandPassFilter and related classes; implement new filter structure

--- Assets/Code/Scripts/CSound/CsoundController.cs
+++ Assets/Code/Scripts/CSound/CsoundController.cs
@@ -15,52 +15,12 @@ public class CsoundController : MonoBehaviour
 
     [SerializeField] private readonly bool updateEveryFrame = true;
 
-    readonly FrequencyRange freq125HzRange = new(Frequency.freq125Hz, Frequency.freq0Hz);
-    readonly FrequencyRange freq250HzRange = new(Frequency.freq250Hz, Frequency.freq125Hz);
-    readonly FrequencyRange freq500HzRange = new(Frequency.freq500Hz, Frequency.freq250Hz);
-    readonly FrequencyRange freq1000HzRange = new(Frequency.freq1000Hz, Frequency.freq500Hz);
-    readonly FrequencyRange freq2000HzRange = new(Frequency.freq2000Hz, Frequency.freq1000Hz);
-    readonly FrequencyRange freq4000HzRange = new(Frequency.freq4000Hz, Frequency.freq2000Hz);
-
-    readonly ButterBandPassFilter[] leftBands = new ButterBandPassFilter[6];
-    readonly ButterBandPassFilter[] rightBands = new ButterBandPassFilter[6];
-
-    // Probably what will do in another class
-    void OnAudioFilterRead(float[] data, int channels)
-    {
-        for (int i = 0; i < data.Length; i += channels)
-        {
-            float inputL = data[i];
-            float inputR = data[i + 1];
-
-            float outL = 0f;
-            float outR = 0f;
-
-            outL += leftBands[0].Process(inputL, freq125HzRange.centerFreq, freq125HzRange.bandWidth);
-            outR += rightBands[0].Process(inputR, freq125HzRange.centerFreq, freq125HzRange.bandWidth);
-
-            outL += leftBands[1].Process(inputL, freq250HzRange.centerFreq, freq250HzRange.bandWidth);
-            outR += rightBands[1].Process(inputR, freq250HzRange.centerFreq, freq250HzRange.bandWidth);
-
-            outL += leftBands[2].Process(inputL, freq500HzRange.centerFreq, freq500HzRange.bandWidth);
-            outR += rightBands[2].Process(inputR, freq500HzRange.centerFreq, freq500HzRange.bandWidth);
-
-            outL += leftBands[3].Process(inputL, freq1000HzRange.centerFreq, freq1000HzRange.bandWidth);
-            outR += rightBands[3].Process(inputR, freq1000HzRange.centerFreq, freq1000HzRange.bandWidth);
-
-            outL += leftBands[4].Process(inputL, freq2000HzRange.centerFreq, freq2000HzRange.bandWidth);
-            outR += rightBands[4].Process(inputR, freq2000HzRange.centerFreq, freq2000HzRange.bandWidth);
-
-            outL += leftBands[5].Process(inputL, freq4000HzRange.centerFreq, freq4000HzRange.bandWidth);
-            outR += rightBands[5].Process(inputR, freq4000HzRange.centerFreq, freq4000HzRange.bandWidth);
-
-            // outL *= 1f / 6f; // Normalize to prevent clipping (adjust as needed)
-            // outR *= 1f / 6f;
-
-            data[i] = outL;
-            data[i + 1] = outR;
-        }
-    }
+    readonly FrequencyRange freq125HzRange = new(Frequency.freq125Hz);
+    readonly FrequencyRange freq250HzRange = new(Frequency.freq250Hz);
+    readonly FrequencyRange freq500HzRange = new(Frequency.freq500Hz);
+    readonly FrequencyRange freq1000HzRange = new(Frequency.freq1000Hz);
+    readonly FrequencyRange freq2000HzRange = new(Frequency.freq2000Hz);
+    readonly FrequencyRange freq4000HzRange = new(Frequency.freq4000Hz);
 
     void Start()
     {
@@ -94,25 +54,6 @@ public class CsoundController : MonoBehaviour
                 Debug.Log($"CsoundController: AudioSource already playing clip '{source.clip.name}'.");
             }
         }
-    }
-
-    void Update()
-    {
-        if (updateEveryFrame)
-        {
-            PushAbsorption();
-        }
-    }
-
-    void PlaySound()
-    {
-        //initializes and plays the audio source
-
-    }
-
-    void ModifyPlayingSound() // push variables to csound
-    {
-
     }
 
     void PushAbsorption()
